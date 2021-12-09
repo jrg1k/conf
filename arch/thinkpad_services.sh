54c7521e906b3f3afc096fee4faaecf781e33955
Jørgen Kristensen <jk@jkristensen.net>
2021-12-09 23:01:36 +0100

    updated package lists

--- arch/thinkpad_services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-systemctl enable firewalld.service 
-systemctl enable NetworkManager.service 
-systemctl enable apparmor.service 
-systemctl enable fstrim.timer 
-systemctl enable systemd-timesyncd.service 
-systemctl enable gdm.service 
