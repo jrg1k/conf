4ab9d7eed48a415c2e66cab83fb6210cf0832c56
jk <jk@jkristensen.net>
2021-12-14 20:30:17 +0100

    added script to unload/load nouveau on suspend

---
+++ arch/configs/10-nouveau.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+case $1/$2 in
+  pre/*)
+    # echo "Going to $2..."
+    /usr/bin/echo "0" > /sys/class/vtconsole/vtcon1/bind
+    /usr/bin/rmmod nouveau
+    ;;
+  post/*)
+    # echo "Waking up from $2..."
+    /usr/bin/modprobe nouveau
+    /usr/bin/echo "1" > /sys/class/vtconsole/vtcon1/bind
+    ;;
+esac
