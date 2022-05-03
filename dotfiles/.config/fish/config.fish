ddf73c65b703d704ce41b8eb1be987fa8af83420
Jørgen Kristensen <jk@bitnor.org>
2022-05-03 17:19:44 +0200

    cleanup

--- dotfiles/.config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-if test -n "$DESKTOP_SESSION"
-    set -x (gnome-keyring-daemon --start | string split "=")
-end
